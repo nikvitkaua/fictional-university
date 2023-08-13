7c3eea56c7509c9ec0213c509e7110fcd283a1ca
Mykyta Paiuka <nikvitka.it@gmail.com>
2023-08-13 12:46:01 +0300

    add dynamic menu on theme

--- functions.php
+++ functions.php
@@ -12,7 +12,10 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 // Add features
 function university_features() {
-  add_theme_support( 'title-tag' );
+  register_nav_menu('headerMenuLocation', 'Header Menu Location');
+  register_nav_menu('footerLocationOne', 'Footer Location One');
+  register_nav_menu('footerLocationTwo', 'Footer Location Two');
+  add_theme_support('title-tag');
 }
 
 add_action('after_setup_theme', 'university_features');
--- header.php
+++ header.php
@@ -15,13 +15,11 @@
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
-          <ul>
-            <li><a href="/about-us">About Us</a></li>
-            <li><a href="#">Programs</a></li>
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
-          </ul>
+          <?php 
+            wp_nav_menu(array(
+              'theme_location' => 'headerMenuLocation'
+            ));
+          ?>
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
